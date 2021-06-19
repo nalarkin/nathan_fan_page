df4a5d4d269fe8c2d938a9a4415faced243e9474
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-06-18 20:55:23 -0400

    added loading icon

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:fanpage/services/auth.dart';
 import 'package:fanpage/shared/constants.dart';
+import 'package:fanpage/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -14,86 +15,96 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = new GlobalKey<FormState>();
+  bool loading = false;
   // text field state
   String email = '';
   String password = '';
   String error = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.brown[100],
-        appBar: AppBar(
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          title: Text('Sign up to Nates Fans'),
-          actions: <Widget>[
-            TextButton.icon(
-                onPressed: () {
-                  widget.toggleView();
-                },
-                icon: Icon(Icons.person),
-                label: Text("Sign In"))
-          ],
-        ),
-        body: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    validator: (val) {
-                      return (val?.isEmpty as bool) ? 'Enter and email' : null;
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign up to Nates Fans'),
+              actions: <Widget>[
+                TextButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                    onChanged: (val) {
-                      setState(() {
-                        email = val;
-                      });
-                    },
+                    icon: Icon(Icons.person),
+                    label: Text("Sign In"))
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) {
+                          return (val?.isEmpty as bool)
+                              ? 'Enter and email'
+                              : null;
+                        },
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true,
+                        validator: (val) {
+                          return (val?.length as int) < 6
+                              ? 'Enter password 6+ chars long'
+                              : null;
+                        },
+                        onChanged: (val) {
+                          setState(() {
+                            password = val;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.pink[400]),
+                          onPressed: () async {
+                            if (_formKey.currentState?.validate() as bool) {
+                              setState(() => loading = true);
+                              dynamic result =
+                                  await _auth.registerWithEmailAndPassword(
+                                      email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error = 'register with a valid email';
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                          child: Text(
+                            "Register",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ],
                   ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                    obscureText: true,
-                    validator: (val) {
-                      return (val?.length as int) < 6
-                          ? 'Enter password 6+ chars long'
-                          : null;
-                    },
-                    onChanged: (val) {
-                      setState(() {
-                        password = val;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  ElevatedButton(
-                      style:
-                          ElevatedButton.styleFrom(primary: Colors.pink[400]),
-                      onPressed: () async {
-                        if (_formKey.currentState?.validate() as bool) {
-                          dynamic result = await _auth
-                              .registerWithEmailAndPassword(email, password);
-                          if (result == null) {
-                            setState(() {
-                              error = 'register with a valid email';
-                            });
-                          }
-                        }
-                      },
-                      child: Text(
-                        "Register",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                  SizedBox(height: 12.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  )
-                ],
-              ),
-            )));
+                )));
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:fanpage/services/auth.dart';
 import 'package:fanpage/shared/constants.dart';
+import 'package:fanpage/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -14,6 +15,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = new GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -22,82 +24,89 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.brown[100],
-        appBar: AppBar(
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          title: Text('Sign in to Nates Fans'),
-          actions: <Widget>[
-            TextButton.icon(
-                onPressed: () {
-                  widget.toggleView();
-                },
-                icon: Icon(Icons.person),
-                label: Text("Register"))
-          ],
-        ),
-        body: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    validator: (val) {
-                      return (val?.isEmpty as bool) ? 'Enter and email' : null;
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign in to Nates Fans'),
+              actions: <Widget>[
+                TextButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                    onChanged: (val) {
-                      setState(() {
-                        email = val;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration:
-                        textInputDecoration.copyWith(hintText: 'Password'),
-                    obscureText: true,
-                    validator: (val) {
-                      return (val?.length as int) < 6
-                          ? 'Enter password 6+ chars long'
-                          : null;
-                    },
-                    onChanged: (val) {
-                      setState(() {
-                        password = val;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  ElevatedButton(
-                      style:
-                          ElevatedButton.styleFrom(primary: Colors.pink[400]),
-                      onPressed: () async {
-                        if (_formKey.currentState?.validate() as bool) {
-                          dynamic result = await _auth
-                              .signInWithEmailAndPassword(email, password);
+                    icon: Icon(Icons.person),
+                    label: Text("Register"))
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) {
+                          return (val?.isEmpty as bool)
+                              ? 'Enter and email'
+                              : null;
+                        },
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        obscureText: true,
+                        validator: (val) {
+                          return (val?.length as int) < 6
+                              ? 'Enter password 6+ chars long'
+                              : null;
+                        },
+                        onChanged: (val) {
+                          setState(() {
+                            password = val;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.pink[400]),
+                          onPressed: () async {
+                            if (_formKey.currentState?.validate() as bool) {
+                              setState(() => loading = true);
+                              dynamic result = await _auth
+                                  .signInWithEmailAndPassword(email, password);
 
-                          if (result == null) {
-                            setState(() {
-                              error = 'Incorrect login and/or password.';
-                            });
-                          }
-                        }
-                      },
-                      child: Text(
-                        "Sign in",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                  SizedBox(height: 12.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  )
-                ],
-              ),
-            )));
+                              if (result == null) {
+                                setState(() {
+                                  error = 'Incorrect login and/or password.';
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                          child: Text(
+                            "Sign in",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ],
+                  ),
+                )));
   }
 }
